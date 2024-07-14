dc034abfbc8ecb19e4465a19f1cfe8440e2d1e88
Kristina <kkristina.kolesnyk@gmail.com>
2024-07-14 14:33:22 +0300

    error check

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class ErrorBoundry extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({hasError: true});
+        console.error('Error Boundary caught an error:', error, info);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+        return this.props.children;
+    }
+
+}
+
+export default ErrorBoundry;
